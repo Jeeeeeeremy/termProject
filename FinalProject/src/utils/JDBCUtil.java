8d9092195fd5c211911d308a93c447e5f94b9477
yx-nie <nie.yo@northeastern.edu>
2022-12-05 14:26:08 -0500

    hospital

--- FinalProject/src/utils/JDBCUtil.java
+++ FinalProject/src/utils/JDBCUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 
 public class JDBCUtil {
 
-    private static final String URL="jdbc:mysql://localhost:3306/final";
+    private static final String URL="jdbc:mysql://localhost:3306/final?autoReconnect=true&useSSL=false";
     private static final String NAME="root";
     private static final String PASSWORD="rootrootroot";
 
@@ -16,7 +16,7 @@ public class JDBCUtil {
     static{
         try {
             //1.加载驱动程序
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             //2.获得数据库的连接
             conn = DriverManager.getConnection(URL, NAME, PASSWORD);
         } catch (ClassNotFoundException e) {
@@ -28,14 +28,5 @@ public class JDBCUtil {
     //对外提供一个方法来获取数据库连接
     public static Connection getConnection(){
         return conn;
-    }
-    
-    public static void close(Connection conn){
-        try {
-            conn.close();
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
     }
 }
