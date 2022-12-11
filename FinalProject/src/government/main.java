723ea74f284722cab4647d7772c1882d739ccba0
jeeeeeeremy <wang.yibin@northeastern.edu>
2022-12-11 13:33:14 -0500

    send email&government

---
+++ FinalProject/src/government/main.java
@@ -0,0 +1,40 @@
+import java.awt.*;
+import javax.swing.*;
+import org.jdesktop.layout.GroupLayout;
+/*
+ * Created by JFormDesigner on Sun Dec 11 13:33:02 EST 2022
+ */
+
+
+
+/**
+ * @author unknown
+ */
+public class main extends JFrame {
+    public main() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+        //======== this ========
+        Container contentPane = getContentPane();
+
+        GroupLayout contentPaneLayout = new GroupLayout(contentPane);
+        contentPane.setLayout(contentPaneLayout);
+        contentPaneLayout.setHorizontalGroup(
+            contentPaneLayout.createParallelGroup()
+                .add(0, 400, Short.MAX_VALUE)
+        );
+        contentPaneLayout.setVerticalGroup(
+            contentPaneLayout.createParallelGroup()
+                .add(0, 300, Short.MAX_VALUE)
+        );
+        pack();
+        setLocationRelativeTo(getOwner());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
+}
